0cc340177359ee04df0a6c38d1b218964fa13a8d
suman1shrestha <sumannshrestha@gmail.com>
2016-03-18 18:07:52 +0000

    <final version>

--- homework/calculator.s
+++ homework/calculator.s
@@ -12,7 +12,7 @@
 
 main:
 	BL _scanf				@ branch to scanf prodecure with return
-	MOV R9, R0
+	MOV R9, R0				
 	BL _getchar
 	MOV R10, R0
 	BL _scanf
@@ -29,43 +29,43 @@ _getchar:
 	MOV R7, #3 				@ write syscall, 4
 	MOV R0, #0				@ output stream to monitor, 1
 	MOV R2, #1				@ read a single character
-	LDR R1, =read_char		@ store the character in data memory
-	SWI 0					@ excute the system call
-	LDR R0, [R1]			@ move the character to the return register
-	AND R0, #0xFF			@ mask out all but the lowest 8 bits
+	LDR R1, =read_char			@ store the character in data memory
+	SWI 0					@ execute the system call
+	LDR R0, [R1]				@ move the character to the return register
+	AND R0, #0xFF				@ mask out all but the lowest 8 bits
 	MOV PC, LR 				@ return
 
 _scanf:
-	MOV R4, LR 			@ store LR since scanf call overwrites
-	SUB SP, SP, #4			@ make romm on stack
-	LDR R0, =format_str		@ R0 contains address of format string
+	MOV R4, LR 				@ store LR since scanf call overwrites
+	SUB SP, SP, #4				@ make romm on stack
+	LDR R0, =format_str			@ R0 contains address of format string
 	MOV R1, SP 				@ move SP to R1 to store entry on stack
 	BL scanf 				@ call scanf
-	LDR R0, [SP]  			@ load value at SP into R0
-	ADD SP, SP, #4			@ restore the stack pointer
+	LDR R0, [SP]  				@ load value at SP into R0
+	ADD SP, SP, #4				@ restore the stack pointer
 	MOV PC, R4				@ return
 
 _compare:
 	MOV R4, LR
-	CMP R2, #'+'			@ compare against the constant char '+'
+	CMP R2, #'+'				@ compare against the constant char '+'
 	BLEQ _add				@ branch to equal handler
-	CMP R2, #'-'			@ compare against the constatn char '-'
+	CMP R2, #'-'				@ compare against the constatn char '-'
 	BLEQ _sub				@ branch to equal handler
-	CMP R2, #'*'			@ compare against the constatn char '*'
+	CMP R2, #'*'				@ compare against the constatn char '*'
 	BLEQ _mult				@ branch to equal handler
-	CMP R2, #'M'			@ compare against the constatn char 'M'
+	CMP R2, #'M'				@ compare against the constatn char 'M'
 	BLEQ _max				@ branch to equal handler
 	MOV PC, R4
 
 _printf:
 	MOV R4, LR 				@ store LR since printf call overwrites
-	LDR R0, =print_str		@ R0 contains formatted string address
+	LDR R0, =print_str			@ R0 contains formatted string address
 	MOV R1, R8				@ R8 contains printf argument (redundant line)
 	BL printf 				@ call printf
 	MOV PC, R4				@ return
 
 _add: 
-	ADD R0, R1, R3
+	ADD R0, R1, R3				
 	MOV PC, LR
 
 _sub:
