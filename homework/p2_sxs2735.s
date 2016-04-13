b886ba9706f1f47731f1081ff0ce056306420fa5
suman1shrestha <sumannshrestha@gmail.com>
2016-04-12 21:54:14 -0500

    Update p2_sxs2735.s

--- homework/p2_sxs2735.s
+++ homework/p2_sxs2735.s
@@ -15,53 +15,52 @@
 main:
 	BL _scanf				@ branch to scanf prodecure with return
 	MOV R9, R0				@ move return value R0 to argument register R9
-	MOV R5, R0
+	MOV R5, R0				@ store n in R5
 	BL _scanf              			@ branch to scanf prodecure with return
-	MOV R10, R0            			@ move return value R0 to argument register R11
-	MOV R6, R0
+	MOV R10, R0            			@ move return value R0 to argument register R10
+	MOV R6, R0				@ store n in R6
 	MOV R1, R9             			@ move return value R9 to argument register R1
-	MOV R2, R10            			@ move return value R11 to argument register R3
-	BL count_partitions            			@ branch to compare prodecure with return
-	MOV R1, R0
-	MOV R2, R5
-	MOV R3, R6             			@ move return value R0 to argument register R8
+	MOV R2, R10            			@ move return value R10 to argument register R2
+	BL count_partitions            		@ branch to count_partitions prodecure with return
+	MOV R1, R0				@ pass result to printf procedure
+	MOV R2, R5				@ pass n to printf procedure
+	MOV R3, R6             			@ pass m to printf procedure
 	BL _printf             			@ branch to printf prodecure with return
 	B main                 			@ branch to main prodecure with no return
 
 count_partitions:
-	PUSH {LR}
-	CMP R1, #0
-	MOVEQ R0, #1
-	POPEQ {PC}
+	PUSH {LR}				@ store the return address
+	CMP R1, #0				@ if (n == 0)
+	MOVEQ R0, #1				@ R0 = 1
+	POPEQ {PC}				@ return R0
 
-	CMP R1, #0
-	MOVLT R0, #0
-	POPLT {PC}
+	CMP R1, #0				@ else if (n<0)
+	MOVLT R0, #0				@ R0 = 0
+	POPLT {PC}				@ return R0
 
-	CMP R2, #0
-	MOVEQ R0, #0
-	POPEQ {PC}
-
-	PUSH {R1}
-	PUSH {R2}
-	@MOV R0, R2
-	@MOV R2, R1
-	SUB R2, R2, #1
-	BL count_partitions
-	MOV R11, R0
-	POP {R2}
-	POP {R1}
-	PUSH {R1}
-	PUSH {R2}
-	PUSH {R11}
-	SUB R1, R1, R2
-	BL count_partitions
-	POP {R11}
-	POP {R2}
-	POP {R1}
-	ADD R11, R11, R0
-	MOV R0, R11
-	POP {PC}
+	CMP R2, #0				@ else if (m == 0)
+	MOVEQ R0, #0				@ R0 = 0
+	POPEQ {PC}				@ return R0
+	
+	@ For Recursion
+	PUSH {R1}				@ backup input argument value, n
+	PUSH {R2}				@ backup input argument value, m
+	SUB R2, R2, #1				@ (m-1) into R2
+	BL count_partitions			@ compute count_partitions(n,m-1)
+	MOV R11, R0				@ store the result in R11
+	@POP {R2}				@ restore the input argument, m
+	@POP {R1}				@ restore the input argument, n
+	@PUSH {R1}				@ backup the input argument, n
+	@PUSH {R2}				@ backup the input argument, m
+	@PUSH {R11}				@ backup the result from count_partitions(n,m-1)
+	SUB R1, R1, R2				@ (n-m) into R1
+	BL count_partitions			@ compute count_partitions(n-m,m)
+	@POP {R11}				@ restore the result from count_partitions(n,m-1)
+	POP {R2}				@ restore the input argument, m
+	POP {R1}				@ restore the input argument, n
+	ADD R11, R11, R0			@ compute count_partitions(n-m,m) + count_partitions(n,m-1)
+	MOV R0, R11				@ move the return result to R0
+	POP {PC}				@ restore the stack pointer and return
 
 _scanf:
 	MOV R4, LR 				@ store LR since scanf call overwrites
