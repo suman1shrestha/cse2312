387be964c8d47da05bf888d7d19fd05ee20cb34c
suman1shrestha <sumannshrestha@gmail.com>
2016-04-27 16:36:43 -0500

    Update p3_sxs2735.s

--- homework/p3_sxs2735.s
+++ homework/p3_sxs2735.s
@@ -26,13 +26,13 @@ generate:
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
     ADD R5, R3, R0
-    STR R2, [R5]              @ write the address of a[i] to a[i]
-    @ADD R4, R0, #1
-    @ADD R6, R3, R4
+    STR R5, [R2]              @ write the address of a[i] to a[i]
+    ADD R4, R0, #1
+    ADD R6, R3, R4
     MOV R8, #0
-    @SUB R6, R8, R6
-    @ADD R2, R2, #4
-    @STR R2, [R6]
+    SUB R6, R8, R6
+    ADD R2, R2, #4
+    STR R6, [R2]
     ADD R0, R0, #2          @ increment index
     B generate              @ branch to next loop iteration
 generatedone:
