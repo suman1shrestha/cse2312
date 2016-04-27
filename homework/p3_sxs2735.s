9ee392d7f2d4e0bd9abbe7cbc9b8f3135b2d0ed3
suman1shrestha <sumannshrestha@gmail.com>
2016-04-27 16:22:20 -0500

    Update p3_sxs2735.s

--- homework/p3_sxs2735.s
+++ homework/p3_sxs2735.s
@@ -24,7 +24,7 @@ generate:
     BEQ generatedone           @ exit loop if done
     LDR R1, =a              @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
-    ADD R2, R1, R2          @ R2 now has the element address
+    @ADD R2, R1, R2          @ R2 now has the element address
     ADD R5, R3, R0
     STR R2, [R5]              @ write the address of a[i] to a[i]
     ADD R4, R0, #1
