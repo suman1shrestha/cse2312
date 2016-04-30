704e970e679c48b3c276e4885c9612a8ec045051
suman1shrestha <sumannshrestha@gmail.com>
2016-04-30 14:47:34 -0500

    Update problem_3.s

--- homework/problem_3.s
+++ homework/problem_3.s
@@ -38,23 +38,23 @@ generate:
 generatedone:
     MOV R0, #0              @ initialze index variable
    @ MOV R6, #0
-    MOV R10, #0
+    @MOV R10, #0
     
 _sort:
     CMP R0, #20           
     BEQ sortDone 
-    ADD R6, R0, #1
+   @ ADD R6, R0, #1
     LDR R1, =a  
     LSL R2, R0, #2     
     ADD R2, R1, R2   
-    LSL R7, R6, #2
-    ADD R7, R1, R7
+    @LSL R7, R6, #2
+   @ ADD R7, R1, R7
     LDR R1, [R2]            @ load contents of a into R1
-    LDR R8, [R7]
-    CMP R8, R1
-    MOVLT R5, R1
-    MOVLT R1, R8
-    MOVLT R8, R5
+    @LDR R8, [R7]
+    @CMP R8, R1
+   @ MOVLT R5, R1
+    @MOVLT R1, R8
+    @MOVLT R8, R5
     LDR R3, =b              @ load b
     LSL R4, R0, #2         @ set the address
     ADD R4, R3, R4          @ add b address to R4
