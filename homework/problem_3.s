ab7fc66ba60e3bf2bc3bf4ffb60bada6a02de66b
suman1shrestha <sumannshrestha@gmail.com>
2016-04-30 16:23:35 -0500

    Update problem_3.s

--- homework/problem_3.s
+++ homework/problem_3.s
@@ -41,12 +41,11 @@ generatedone:
     @MOV R10, #0
     
     
-_sort:
+_copy:
     CMP R0, #20           
     BEQ sortDone 
-    ADD R6, R0, #1
     LDR R1, =a  
-    LSL R2, R6, #2     
+    LSL R2, R0, #2     
     ADD R2, R1, R2     
     LDR R1, [R2]            @ load contents of a into R1
     LDR R3, =b              @ load b
@@ -55,9 +54,9 @@ _sort:
     MOV R5, R1
     ADD R0, R0, #1        @ increment the counter 
     STR R5, [R4]            @ store the contents of R5 into b
-    B _sort           
+    B _copy           
     
-sortDone:
+sortCopy:
     MOV R0, #0    
    
 readLoop:
