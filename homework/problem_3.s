5ce91eb0a5e20ce80ba995a5ebe1a47d4d4b2b10
suman1shrestha <sumannshrestha@gmail.com>
2016-04-29 21:12:17 -0500

    Update problem_3.s

--- homework/problem_3.s
+++ homework/problem_3.s
@@ -43,9 +43,9 @@ generatedone:
 _sort:
     CMP R0, #20           
     BEQ sortDone 
-   @ADD R6, R0, #1
+    ADD R6, R0, #1
     LDR R1, =a  
-    LSL R2, R0, #2     
+    LSL R2, R6, #2     
     ADD R2, R1, R2     
     LDR R1, [R2]            @ load contents of a into R1
     LDR R3, =b              @ load b
