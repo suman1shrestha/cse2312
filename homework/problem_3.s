7ef0b5b7e16663f9348314cf5c7e53d78e45b7ca
suman1shrestha <sumannshrestha@gmail.com>
2016-04-30 17:24:32 -0500

    Update problem_3.s

--- homework/problem_3.s
+++ homework/problem_3.s
@@ -95,7 +95,7 @@ _sorted:
     ADD R4, R3, R4          @ add b address to R4
     STR R1, [R2]
     STR R8, [R4]            @ store the contents of R5 into b
-    MOV R5, R8
+    MOV R5, R1
     ADD R0, R0, #1        @ increment the counter 
     B _sort           
     
