262729ff42ded6767ab89efbebd227c5d7e06df5
suman1shrestha <sumannshrestha@gmail.com>
2016-05-07 16:30:33 -0500

    Update px_sxs2735.s

--- homework/px_sxs2735.s
+++ homework/px_sxs2735.s
@@ -14,38 +14,52 @@
 .func main
 
 main:
+    MOV R0, #0
+    MOV R3, #0
     BL _generate
     MOV R8, R0
     LDR R0, =printf_str
     MOV R1, R8
-    BL _getMin
+    BL _printMyArray
     MOV R1, R0
-    BL _printMin
+    BL _printAdd
     BL _getMax
     MOV R1, R0
-    BL _printMax
-    BL _getSum
-    MOV R1, R0
-    BL _printSum
-    B _exit
+    BL _printAdd
+    BL _exit
 
 
 _generate:
     PUSH {LR}
-    MOV R4, #0         @ i = 0
     
+    MOV R4, #0         @ i = 0
     writeloop:
     CMP R4, #10        @ if (i <10)
     POPEQ {PC}         @ if R3 = 10, leave
-    LDR R6, =array_a   @address of a_array
+    LDR R6, =a_array   @address of a_array
     LSL R7, R4, #2
     ADD R7, R6, R7
     BL _scanf          @ get user input
     MOV R5, R0         @ puts user input to R8
     STR R5, [R7]       @ a_array[i] = R8
     ADD R4, R4, #1     @ i++;
+    
     B writeloop
+    
 
+#27
+_exit:  
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #21             @ print string length
+    LDR R1, =exit_str       @ string at label exit_str:
+    SWI 0                   @ execute syscall
+    MOV R7, #1              @ terminate syscall, 1
+    SWI 0                   @ execute syscall
+
+
+#38
+#47
 _printf:
     PUSH {LR}               @ store LR since printf call overwrites
     LDR R0, =printf_str     @ R0 contains formatted string address
@@ -54,39 +68,29 @@ _printf:
     MOV R3, R3
     BL printf               @ call printf
     POP {PC}                @ return
-    
-_printSum:
+#54
+_printAdd:
     PUSH {LR}               @ store LR since printf call overwrites
-    LDR R0, =printf_Sum     @ R0 contains formatted string address
+    LDR R0, =printf_Add     @ R0 contains formatted string address
     MOV R1, R1              @
     BL printf               @ call printf
     POP {PC}                @ return
 
-_printMax:
-    PUSH {LR}               @ store LR since printf call overwrites
-    LDR R0, =printf_Max     @ R0 contains formatted string address
-    MOV R1, R1              @
-    BL printf               @ call printf
-    POP {PC}                @ return
-    
-_printMin:
-    PUSH {LR}               @ store LR since printf call overwrites
-    LDR R0, =printf_Min     @ R0 contains formatted string address
-    MOV R1, R1              @
-    BL printf               @ call printf
-    POP {PC}                @ return
 
-_getMin:
+
+#80
+_printMyArray:
     PUSH {LR}
-    BL _getMax
-    MOV R10, R0             @ max = 0
     MOV R0, #0              @ i = 0
+    MOV R8, #0              @ sum = 0
+    MOV R9, #0              @ max = 0
+    MOV R10, #900          @ min = 0
 
     readloop:
     CMP R0, #10             @ check to see if we are done iterating
-    MOVEQ R0, R10
-    POPEQ {PC}
-    LDR R1, =array_a        @ get address of a
+    MOVEQ R0, R8
+    POPEQ {PC}              @ exit loop if done
+    LDR R1, =a_array        @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
     LDR R1, [R2]            @ read the array at address
@@ -95,9 +99,8 @@ _getMin:
     PUSH {R2}               @ backup register before printf
     MOV R2, R1              @ move array value to R2 for printf
     MOV R1, R0              @ move array index to R1 for printf
+    ADD R8, R8, R2          @ sum+= a_array[i]
     BL  _printf             @ branch to print procedure with return
-    CMP R2, R10              @ sum+= a_array[i]
-    MOVLT R10, R2
     POP {R2}                @ restore register
     POP {R1}                @ restore register
     POP {R0}                @ restore register
@@ -113,7 +116,7 @@ _getMax:
     CMP R0, #10             @ check to see if we are done iterating
     MOVEQ R0, R9
     POPEQ {PC}              @ exit loop if done
-    LDR R1, =array_a        @ get address of a
+    LDR R1, =a_array        @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
     LDR R1, [R2]            @ read the array at address
@@ -121,23 +124,9 @@ _getMax:
     MOVGT R9, R1
     ADD R0, R0, #1          @ increment index
     B   maxloop             @ branch to next loop iteration
-    
-_getSum:
-    PUSH {LR}
-    MOV R0, #0              @ i = 0
-    MOV R8, #0              @ max = 0
 
-    sumloop:
-    CMP R0, #10             @ check to see if we are done iterating
-    MOVEQ R0, R8
-    POPEQ {PC}              @ exit loop if done
-    LDR R1, =array_a        @ get address of a
-    LSL R2, R0, #2          @ multiply index*4 to get array offset
-    ADD R2, R1, R2          @ R2 now has the element address
-    LDR R1, [R2]            @ read the array at address
-    ADD R8, R8, R0          @ sum+= a_array[i]
-    ADD R0, R0, #1          @ increment index
-    B   sumloop             @ branch to next loop iteration
+    
+
 
 _scanf:
     PUSH {LR}               @ store LR since scanf call overwrites
@@ -149,23 +138,15 @@ _scanf:
     ADD SP, SP, #4          @ restore the stack pointer
     POP {PC}
 
-_exit:  
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R2, #21             @ print string length
-    LDR R1, =exit_str       @ string at label exit_str:
-    SWI 0                   @ execute syscall
-    MOV R7, #1              @ terminate syscall, 1
-    SWI 0                   @ execute syscall
+
 
 
 .data
 
 .balign 4
-array_a:              .skip     40
-printf_str:     .asciz    "array_a[%d] = %d\n"
-printf_Sum:     .asciz    "sum = %d\n"
+a_array:              .skip     40
+printf_str:     .asciz    "a_array[%d] = %d\n"
+printf_Add:     .asciz    "sum = %d\n"
 format_str:     .asciz    "%d"
-printf_Max:      .ascii   "Maximum = %d\n"
-printf_Min:      .ascii   "Minimum = %d\n"
+prompt_str:      .ascii   "Enter the @ character: "
 exit_str:       .ascii    "Terminate program.\n"
