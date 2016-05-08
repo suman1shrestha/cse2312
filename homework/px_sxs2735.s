81bf8f734c686ba537af302387d2a3386481f2af
suman1shrestha <sumannshrestha@gmail.com>
2016-05-08 14:01:19 -0500

    Update px_sxs2735.s

--- homework/px_sxs2735.s
+++ homework/px_sxs2735.s
@@ -131,6 +131,15 @@ _scanf:
     ADD SP, SP, #4          @ restore the stack pointer
     POP {PC}
 
+_exit:  
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #21             @ print string length
+    LDR R1, =exit_str       @ string at label exit_str
+    SWI 0                   @ execute syscall
+    MOV R7, #1              @ terminate syscall, 1
+    SWI 0                   @ execute syscall
+    
 _printf:
     PUSH {LR}               @ store LR since printf call overwrites
     LDR R0, =printf_str     @ R0 contains formatted string address
@@ -143,14 +152,6 @@ _printResults:
     BL printf               @ call printf
     POP {PC}                @ return
 
-_exit:  
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R2, #21             @ print string length
-    LDR R1, =exit_str       @ string at label exit_str
-    SWI 0                   @ execute syscall
-    MOV R7, #1              @ terminate syscall, 1
-    SWI 0                   @ execute syscall
     
     
 .data
